9aed58782bf5fb08672bc923594df4710e49f12b
jvoyatz <jvoyatz@gmail.com>
2024-03-03 17:43:42 +0200

    added a new constants file

---
+++ core/common/src/main/java/dev/jvoyatz/modern/android/common/constants/Constants.kt
@@ -0,0 +1,14 @@
+package dev.jvoyatz.modern.android.common.constants
+
+//general purpose constants
+const val EMPTY = ""
+const val DASH_WITH_SPACE = " - "
+const val DOTS = "..."
+
+//Date Constants
+const val ISO_DATE_TIME_ZONE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssX"
+const val DATE_FORMAT = "dd-MM-yyyy"
+const val TIME_FORMAT = "HH:mm:ss"
+const val DATE_TIME = "yyyy-MM-dd HH:mm:ss"
+const val DATE_FORMAT_2 = "dd MMMM yyyy"
+const val DATE_FORMAT_3 = "MMMM yyyy"
