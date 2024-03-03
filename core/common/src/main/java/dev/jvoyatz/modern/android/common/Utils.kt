6d619e24ce63bd83c35778a72293a85d81640a29
jvoyatz <jvoyatz@gmail.com>
2024-03-03 17:18:19 +0200

    Added a Utils class in core/common with two top-level-functions

---
+++ core/common/src/main/java/dev/jvoyatz/modern/android/common/Utils.kt
@@ -0,0 +1,22 @@
+package dev.jvoyatz.modern.android.common
+
+
+/**
+ * Accepts a list of type [I] and by applying the [mapper] on each item of this list,
+ * it returns finally a new list with the mapped type
+ *
+ * @param I the type of the input [List]
+ * @param O the type of the output [List]
+ * @param input the [input] that will be mapped into another type [O]
+ * @param mapper contains the code that will transform type [I] to [O]
+ * @return a new list of type [O]
+ */
+inline fun <I, O> mapList(input: List<I>?, mapper: (I) -> O): List<O> {
+    return input?.map { mapper(it) } ?: emptyList()
+}
+
+/**
+ * Same as the [mapList] but it is an extension function on
+ * a receiver object, a list of type [I]
+ */
+inline fun <I, O> List<I>?.mapList(mapper: (I) -> O): List<O> = mapList(this, mapper)
